6e884bf86450667e3ff03b33ee7590a1c58db234
i-am-riya <mriya99s@yahoo.com>
2023-09-04 17:53:23 +0530

    update2

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ card1Btn.addEventListener("click", () => {
   card2.style.display = "block";
 });
 
-
+let rate = null;
 const stars = document.querySelectorAll(".circles");
 const ratingValue = document.getElementById("ratingValue");
 
 stars.forEach((star) => {
-  star.addEventListener("click", () => {
-    star.style.color = 'var(--primary-Orange)';
-    star.style.background = 'var(--Medium-Grey)';
+  star.addEventListener("click", (e) => {
+    const active = document.querySelector(".checked")
+    if(active){
+      active.classList.remove("checked")
+    }
+    const card = e.target;
+    card.classList.add("checked");
+    rate = e.target.innerText;
+
     let rating = parseInt(star.getAttribute('data-value'));
     console.log(rating);
     ratingValue.textContent = `${rating}`;
